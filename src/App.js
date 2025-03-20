f712504f2e8e2f91f82ec02151195bb828eb2781
zaZZa564 <anus_bobra564@mail.ru>
2025-03-20 22:41:52 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 class App extends React.Component {
   render() {
     return(
-      <div>placeholder</div>
+      <div className="App">
+        <Header />
+      </div>
     );
   }
 }
