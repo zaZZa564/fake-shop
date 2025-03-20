ce6573d5eeab6e8809e8d11e16d716bf9e7b708c
zaZZa564 <anus_bobra564@mail.ru>
2025-03-20 23:13:53 +0300

    recap

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from "react";
 class Header extends React.Component {
   render() {
     return(
-      <header className="header">placeholder</header>
+      <header className="header">
+        placeholder
+      </header>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import './style/style.css'
+import './style/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
