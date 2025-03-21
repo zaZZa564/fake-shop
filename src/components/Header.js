4793404f9661fed5ba8695d218d346da54b6883c
zaZZa564 <anus_bobra564@mail.ru>
2025-03-21 11:45:59 +0300

    pointer fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,18 @@ class Header extends React.Component {
     return(
       <header className="header">
         <div className="headerEl logo">
-          <img src={logo} alt="logo" className="logo-svg"></img>
-          <div className="logo-placeholder">logo</div>
+          <img src={logo} alt="logo" className="logo-svg header-content"></img>
+          <div className="logo-placeholder header-content">logo</div>
         </div>
         <input placeholder="search" className="headerEl search"></input>
-        <div className="headerEl about">about us</div>
-        <div className="headerEl sing-in">sing in</div>
+        <div className="headerEl">
+          <div className="header-content about">about us</div>
+        </div>
+        <div className="headerEl sing-in">
+          <div className="header-content">
+            sing in
+          </div>
+        </div>
       </header>
     );
   }
