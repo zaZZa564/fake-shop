f67288e5cb152b0c6f9751c61216628dc0feda93
zaZZa564 <anus_bobra564@mail.ru>
2025-03-21 11:31:16 +0300

    header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
+import logo from '../img/logo.svg';
 
 class Header extends React.Component {
   render() {
     return(
       <header className="header">
-        <div className="headerEl logo">logo</div>
-        <div className="headerEL search-container">
-          <input placeholder="search" className="search"></input>
+        <div className="headerEl logo">
+          <img src={logo} alt="logo" className="logo-svg"></img>
+          <div className="logo-placeholder">logo</div>
         </div>
+        <input placeholder="search" className="headerEl search"></input>
         <div className="headerEl about">about us</div>
         <div className="headerEl sing-in">sing in</div>
       </header>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import './style/style.css';
+import './style/header.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
