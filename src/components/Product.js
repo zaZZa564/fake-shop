1850eb38f056c484938ec7b45d818da1a61c0ec2
zaZZa564 <anus_bobra564@mail.ru>
2025-03-23 22:40:09 +0300

    render single product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { productList } from "../fetch";
 
 class Product extends React.Component {
   render() {
     return(
-      <div>placeholder</div>
+      <div className="product">
+        <h3 className="title">
+          {productList[1].title}
+        </h3>
+        <div className="price">
+          price: {productList[1].price}
+        </div>
+        <div className="description">
+          {productList[1].description}
+        </div>
+      </div>
     );
   }
 }
