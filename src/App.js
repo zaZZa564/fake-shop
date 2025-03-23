cf18b5e028c03afd87b71decd2b2df945b0783c0
zaZZa564 <anus_bobra564@mail.ru>
2025-03-23 23:16:39 +0300

    products grid & render all products

--- src/App.js
+++ src/App.js
@@ -7,8 +7,28 @@ class App extends React.Component {
     return(
       <div className="App">
         <Header />
-        <Product  id='1'/>
-        <Product  id='2'/>
+        <div className="product-grid">
+          <Product  id='1'/>
+          <Product  id='2'/>
+          <Product  id='3'/>
+          <Product  id='4'/>
+          <Product  id='5'/>
+          <Product  id='6'/>
+          <Product  id='7'/>
+          <Product  id='8'/>
+          <Product  id='9'/>
+          <Product  id='10'/>
+          <Product  id='11'/>
+          <Product  id='12'/>
+          <Product  id='13'/>
+          <Product  id='14'/>
+          <Product  id='15'/>
+          <Product  id='16'/>
+          <Product  id='17'/>
+          <Product  id='18'/>
+          <Product  id='19'/>
+          <Product  id='20'/>
+        </div>
       </div>
     );
   }
