45e11e0340d7123ff62d4d327611cc907b2e1dfe
zaZZa564 <anus_bobra564@mail.ru>
2025-03-29 22:36:32 +0300

    homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
   render() {
     return(
       <footer className="footer">
-        <div className="footerEl about">about us</div>
+        <div className="footerEl footerAbout">about us</div>
         <div className="footerEl info">some important info</div>
         <div className="footerEl phone">+7 (351) 240-04-40</div>
       </footer>
