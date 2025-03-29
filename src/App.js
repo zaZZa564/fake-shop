f34f1ee6e384ec0363addab801568989c5822672
zaZZa564 <anus_bobra564@mail.ru>
2025-03-29 22:16:03 +0300

    footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Product from "./components/Product";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
   render() {
@@ -29,6 +30,7 @@ class App extends React.Component {
           <Product  id='19'/>
           <Product  id='20'/>
         </div>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../style/footer.css'
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <footer className="footer">
+
+      </footer>
+    );
+  }
+}
+
+export default Footer;
