d2fd872ab7b58b9416e459ff9bd17381014dcca1
zaZZa564 <anus_bobra564@mail.ru>
2025-03-31 21:53:37 +0300

    react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,31 @@
 import React from "react";
+import reactRouter, { Router } from 'react-router-dom';
 import logo from '../img/logo.svg';
 import cart from '../img/cart.svg';
 
 class Header extends React.Component {
   render() {
     return(
-      <header className="header">
-        <div className="headerEl logo">
-          <img src={logo} alt="logo" className="logo-svg header-content"></img>
-          <div className="logo-placeholder header-content">logo</div>
-        </div>
-        <input placeholder="search" className="headerEl search"></input>
-        <div className="headerEl">
-          <div className="header-content about">about us</div>
-        </div>
-        <div className="headerEl cart">
-          <img src={cart} alt="cart" className="header-content"></img>
-          <div className="header-content cart-content">
-            cart
+      <Router>
+        <header className="header">
+          <div className="headerEl logo">
+            <img src={logo} alt="logo" className="logo-svg header-content"></img>
+            <div className="logo-placeholder header-content">logo</div>
           </div>
-        </div>
-      </header>
+          <input placeholder="search" className="headerEl search"></input>
+          <div className="headerEl">
+            <div className="header-content about">
+              about us
+            </div>
+          </div>
+          <div className="headerEl cart">
+            <img src={cart} alt="cart" className="header-content"></img>
+            <div className="header-content cart-content">
+              cart
+            </div>
+          </div>
+        </header>
+      </Router>
     );
   }
 }
