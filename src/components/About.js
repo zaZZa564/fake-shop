bbe74bf06395eba501e44fba03b7f51e5a0b2231
zaZZa564 <anus_bobra564@mail.ru>
2025-04-03 22:41:52 +0300

    cleaR

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from './Header.js';
 import Footer from './Footer.js'
+import '../style/about.css';
 
 class About extends React.Component {
   render() {
@@ -8,17 +9,7 @@ class About extends React.Component {
       <div className="About">
         <Header />
         <div className='aboutContent'>
-          <h1>Fake shop</h1>
-          <h2>My first pet project. Here's some info about how does it work</h2>
-          <h3>Fake shop is a project represents the work of the real online shop. Here's some technologies that were used in it.</h3>
-          <ul>
-            <li>html5</li>
-            <li>css5</li>
-            <li>js</li>
-            <li>git & github</li>
-            <li>node js & npm</li>
-          </ul>
-          <p>You can download this project on the github <a href="https://github.com/zaZZa564/fake-store">page</a></p>
+          
         </div>
         <Footer />
       </div>
