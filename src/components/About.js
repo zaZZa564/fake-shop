cad753948911c5b6ae77d008c584ec1275b177a1
zaZZa564 <anus_bobra564@mail.ru>
2025-04-03 22:55:49 +0300

    about us text

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,25 @@ class About extends React.Component {
       <div className="About">
         <Header />
         <div className='aboutContent'>
-          
+          <h1>Fake shop</h1>
+          <h2>pet project that represents how does real online shop works</h2>
+
+          <h1>activities</h1>
+          <h2>Here you can add things to cart, choose amount and create order. Then you have to wait some time and your order will be delivered. Also you can check rate and reviews of the product and search by the key words</h2>
+
+          <h1>Big stack of techologies</h1>
+          <h2 className="stackH2">in this project I used such technologies as</h2>
+          <ul>
+            <li>html5</li>
+            <li>css5</li>
+            <li>react js</li>
+            <li>node js</li>
+            <li>npm</li>
+            <li>git & github</li>
+          </ul>
+
+          <h1>Download</h1>
+          <h2>To run this project on your desktop you can visit out <a href="https://github.com/zaZZa564/fake-store">page</a></h2>
         </div>
         <Footer />
       </div>
