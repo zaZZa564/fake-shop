b57b3bd8105cf30609e836307bfcf6f17e45ceef
zaZZa564 <anus_bobra564@mail.ru>
2025-04-05 22:04:06 +0300

    cart pref

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import '../style/cart.css';
 
 class Cart extends React.Component {
   render() {
     return(
       <div className="Cart">
-        placeholder
+        <Header />
+        <Footer />
       </div>
     );
   }
