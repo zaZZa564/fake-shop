e3c44e9fb7539320eb8e60df43fc48112b39a6ee
zaZZa564 <anus_bobra564@mail.ru>
2025-04-06 22:25:40 +0300

    cart content html

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+
+// cart content components
 import EmpryCart from "./EmptyCart";
+import CartContent from "./CartContent";
+
+// style
 import '../style/cart.css';
 
 class Cart extends React.Component {
@@ -9,17 +14,6 @@ class Cart extends React.Component {
     return(
       <div className="Cart">
         <Header />
-        <div className="cart-body">
-          <div className="cart-left">
-            <div className="cart-products"></div>
-            <div className="cart-delivery"></div>
-            <div className="cart-down">
-              <div className="cart-payment"></div>
-              <div className="cart-data"></div>
-            </div>
-          </div>
-          <div className="cart-right"></div>
-        </div>
         <Footer />
       </div>
     );
--- src/components/CartContent.js
+++ src/components/CartContent.js
@@ -3,7 +3,17 @@ import React from "react";
 class CartContent extends React.Component {
   render() {
     return(
-      <div>placeholder</div>
+      <div className="cart-body">
+        <div className="cart-left">
+          <div className="cart-products"></div>
+            <div className="cart-delivery"></div>
+            <div className="cart-down">
+              <div className="cart-payment"></div>
+              <div className="cart-data"></div>
+            </div>
+          </div>
+        <div className="cart-right"></div>
+      </div>
     );
   }
 }
